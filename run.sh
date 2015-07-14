a77be06a0d47bc89d4948df7bb42adef3bc279f2
celabMacPro <celabmacpro@celabMacProsPro.wv.cc.cmu.edu>
2015-07-14 19:39:29 -0400

    updated run.sh with median_unique.py and words_tweeted.py

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+
+python ./src/words_tweeted.py -i tweet_input/tweets.txt -o tweet_output/ft1.txt -q
+python ./src/median_unique.py -i tweet_input/tweets.txt -o tweet_output/ft2.txt -q
